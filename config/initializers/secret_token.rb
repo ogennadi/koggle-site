7d508d17e81dd40fe9159b82f1823ce17aa7c7c7
Oge Nnadi <oge@nnadi.org>
2012-09-08 15:41:39 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KanjigameSite::Application.config.secret_token = 'ddb1210203cef2f00a94c46671f5ee8875d2f5759e1d9ba076cb0673e3d16157a25d4948d77002fb19b1c2494423fc935df17b5b3d4eecb04a9b1e2f44721e19'
